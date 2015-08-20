91a1223726c0563773b1c4cb45de99f3b30528a5
Boris Paillard <boris.paillard@gmail.com>
2015-08-20 17:06:25 +0200

    new seed and adding cocktail picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:show, :index, :new, :create]
+  root "cocktails#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
