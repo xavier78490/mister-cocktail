ac3cac7f41fb66bdfe16b2efd191b6b175dc9a9f
Boris Paillard <boris.paillard@gmail.com>
2015-08-20 17:35:04 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,7 +96,7 @@ cocktails = [
   }
 ]
 
-ingredients = %w(lemon ice 'mint leaves' redbull jagermeister sugar tonic gin rhum)
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
 ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
 
