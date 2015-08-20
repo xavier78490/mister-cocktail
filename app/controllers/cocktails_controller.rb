b356cf6c8c3d61b5ad773bd68f7bef68c76812cb
Boris Paillard <boris.paillard@gmail.com>
2015-08-20 16:54:46 +0200

    cocktail scaffold reworked

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,44 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  # GET /cocktails
+  # GET /cocktails.json
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  # GET /cocktails/1
+  # GET /cocktails/1.json
+  def show
+  end
+
+  # GET /cocktails/new
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  # POST /cocktails
+  # POST /cocktails.json
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    respond_to do |format|
+      if @cocktail.save
+        redirect_to cocktail_path(@cocktail)
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+end
